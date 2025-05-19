45184ba4b9e1b63d8f35a3533e359b723fd8baeb
Maxim <maxim2005ua@gmail.com>
2025-05-20 00:41:53 +0300

    Поднятие предметов и бросок на G
    нужно исправить направление броска

---
+++ Assets/Scripts/Items/Fpc_param.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Fpc_param : MonoBehaviour
+{
+    public bool Take = false;
+}
--- Assets/Scripts/Items/Take.cs
+++ Assets/Scripts/Items/Take.cs
@@ -2,21 +2,26 @@ using UnityEngine;
 
 public class Take : MonoBehaviour
 {
-    float distance = 3;
+    float distance = 100;
     public Transform pos;
     private Rigidbody rb;
+    private MeshCollider ms;
+    private Fpc_param fpc_;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        fpc_ = GameObject.Find("Player").GetComponent<Fpc_param>();
     }
 
+    // По нажатию ЛКМ поднимает перемещает предмет в указанную точку(руку)
     void OnMouseDown()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, distance))
+        if (Physics.Raycast(ray, distance) && fpc_.Take == false)
         {
             rb.isKinematic = true;
+            fpc_.Take = true;
             rb.MovePosition(pos.position);
         }
     }
@@ -24,9 +29,17 @@ public class Take : MonoBehaviour
 
     void FixedUpdate()
     {
+        // Бросок предмета на G и следования предмета за персонажем
         if (rb.isKinematic == true)
         {
             this.gameObject.transform.position = pos.position;
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                fpc_.Take = false;
+                rb.useGravity = true;
+                rb.isKinematic = false;
+                rb.AddForce(Camera.main.transform.forward * 500);
+            }
         }
     }
 }
